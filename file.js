a4288f4071e9906aa6cc784dfbb0e4d687827732
mohamed mohsen <105918314+Elfera@users.noreply.github.com>
2022-07-11 19:56:36 +0300

    learn

---
+++ file.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var friends = new Array
+friends [0] ="mohamed";
+friends [1] ="ahmed";
+friends [2] ="soso";
+friends [3] ="eman";
+friends [4] ="hnan";
+alert (friends [1]);
